ffef6dd6b51b3f8e4a7aa82645de518d75acb165
Mohan <shanmohan@gmail.com>
2015-04-26 18:30:23 +0400

    module & directives has been renamed

--- directive.js
+++ directive.js
@@ -1,14 +1,11 @@
 
-angular.module('ek-module',[])
-    .directive('ek-directive', function(){
+angular.module('ek-directives',[]);
+
+angular.module('ek-directives').directive('ekNumericOnly', function(){
         return {
             require: 'ngModel',
             link: function (scope, element, attrs, modelCtrl) {
                 modelCtrl.$parsers.push(function (inputValue) {
-                    // this next if is necessary for when using ng-required on your input.
-                    // In such cases, when a letter is typed first, this parser will be called
-                    // again, and the 2nd time, the value will be undefined
-                    //console.log(inputValue);
                     if (inputValue == undefined) return '';
                     var transformedInput = inputValue.toString().replace(/[^0-9]/g, '');
                     if (transformedInput != inputValue) {
